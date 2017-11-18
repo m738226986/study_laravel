4479565f1548371567eb86dc4bc225484d0beee7
steven <m738226986@163.com>
2017-11-18 15:17:08 +0000

    blade use

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -6,8 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/static.css">
 </head>
 <body>
+    @include('layout._header')
+
     @yield('content')
 
 </body>
--- resources/views/static_pages/index.blade.php
+++ resources/views/static_pages/index.blade.php
@@ -3,6 +3,17 @@
 @section('title','主页')
 
 @section('content')
-    <h1>我是主页</h1>
+    <div class="jumbotron">
+        <h1>呵呵哒</h1>
+        <p class="lead">
+            你现在看到的是首页
+        </p>
+        <p>
+            一切将从这里开始
+        </p>
+        <p>
+            <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
 
 @stop
